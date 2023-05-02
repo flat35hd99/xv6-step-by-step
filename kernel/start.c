e511f2e97825bc87dc9b3ead5d8a5244899cfc5b
flat35hd99 <flat35hd99@gmail.com>
2023-05-02 14:54:08 +0900

    Hello, Worldの改行コードを改善

--- kernel/start.c
+++ kernel/start.c
@@ -5,8 +5,10 @@
 __attribute__ ((aligned (16))) char stack0[4096 * NCPU];
 
 void hello_world() {
-  int n = 14;
-  char *hw_chars = "hello, world\n";
+  int n = 15;
+  char CR = 0x0D;
+  char LF = 0x0A;
+  char hw_chars[15] = {'H', 'e', 'l', 'l', 'o', ',', ' ', 'W', 'o', 'r', 'l', 'd', '!', CR, LF};
   for (int i = 0; i < n; i++)
   {
     uartputc_sync(hw_chars[i]);
