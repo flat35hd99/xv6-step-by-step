fa3974bf235b92308b9ee1975d54ab9292c4c428
flat35hd99 <flat35hd99@gmail.com>
2023-09-08 23:07:25 +0900

    Update the number of harts

--- kernel/defs.h
+++ kernel/defs.h
@@ -1,4 +1,4 @@
-#define NCPU 4
+#define NCPU 5
 
 // uart.c
 void uartputc_sync(int);
--- run.sh
+++ run.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 QEMU=qemu-system-riscv64
-CPUS=2 # start.cで定義
+
+# 仮想マシンのhartの数
+# def.hで定義した値と一致させること
+# jh7110はSモード非対応なコアが1、Sモード対応なコアが4ある
+CPUS=5
 
 $QEMU \
   -machine virt -bios none -kernel kernel/kernel -m 128M -smp $CPUS -nographic \
